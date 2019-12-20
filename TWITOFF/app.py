6d3339e95fa539dd9a4e5881a0c4c77b4e8e8de3
nwilliams030 <nwilliams030@users.noreply.github.com>
2019-12-20 12:48:26 -0500

    updating

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,20 +1,20 @@
-""" CODE FOR OUR APP """
+"""Code for our app"""
 
 from decouple import config
 from flask import Flask, render_template, request
-from .models import DB
+from .models import DB, User
 
-# now make our app factory
+#make our app factory
+
 def create_app():
     app = Flask(__name__)
-
-    # add config for database
+    #add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-
-    # have database know about the app
+    #have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template('base.html')
+        users = User.query.all()
+        return render_template('base.html', title = 'Home', users=users)
     return app
